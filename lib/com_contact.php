7f2565a9f0238535152af6f81a4b786cafe21b2d
Rodrigo E da S S Pereira <rodrigo.emygdio@gmail.com>
2014-08-01 14:24:53 -0300

    Adicionando verficiação se é m comoponente com_contact para a  intergração do contato do Joomla

--- lib/com_contact.php
+++ lib/com_contact.php
@@ -5,14 +5,17 @@ include __DIR__.'/rd_station_lib/rdstation-integracao.php';
 //error_reporting(E_ALL);
 class Com_contact {
     public static function sendLeadForm($token,$lead_name){
-//        echo '<pre>';var_dump(array_key_exists('jform', $_REQUEST));exit;
-        if(array_key_exists('jform', $_REQUEST)){
-//            echo '<pre>';var_dump($lead_name);exit;
+        
+        if(static::isContactJoomlaForm()){
+//echo '<pre>';var_dump($_REQUEST);exit;
             $data = array(
                      'email'    =>   $_REQUEST['jform']['contact_email'],
                      'nome'     =>   $_REQUEST['jform']['contact_name'],
             );
             addLeadConversionToRdstationCrm($token,$lead_name, $data);
         }
+    }
+    static function  isContactJoomlaForm(){
+        return (array_key_exists('jform', $_REQUEST) &&  $_REQUEST['option'] == 'com_contact');
     }
 }
